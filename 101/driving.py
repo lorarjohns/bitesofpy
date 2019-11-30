ee751c3ae811e63c66d91dee5880925b09a58581
Lora Johns <lora.r.johns@gmail.com>
2019-11-30 00:43:57 -0500

    submission Bite 101 @ codingchalleng.es

---
+++ 101/driving.py
@@ -0,0 +1,8 @@
+MIN_DRIVING_AGE = 18
+
+
+def allowed_driving(name, age):
+    """Print '{name} is allowed to drive' or '{name} is not allowed to drive'
+       checking the passed in age against the MIN_DRIVING_AGE constant"""
+    neg = ' not ' if age < 18 else ' '
+    print(f'{name} is{neg}allowed to drive')
---
+++ newrepo.sh
@@ -0,0 +1,6 @@
+echo "# bitesofpy" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/lorarjohns/bitesofpy.git
+git push -u origin master
